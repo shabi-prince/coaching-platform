282d3a66670dca4de23a56296c5515fc6650a6c5
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 16:54:03 +0500

    Create ItemPurchaseGoods.cs

---
+++ Domain /Entities/ItemPurchaseGoods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ItemPurchaseGoods : GenericModel
+    {
+        public int quantity { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal UnitPrice { get; set; }
+        [ForeignKey("ItemPurchaseOrder")]
+        public int? PurchaseOrderId { get; set; }
+        [ForeignKey("NegatePurchaseOrder")]
+        public int? NegatePurchaseOrderId { get; set; }
+        [ForeignKey("Item")]
+        public int ItemId { get; set; }
+        public virtual Item? Item { get; set; }
+        public virtual ItemPurchaseOrder? ItemPurchaseOrder { get; set; }
+        public virtual NegatePurchaseOrder? NegatePurchaseOrder { get; set; }
+
+    }
+}
