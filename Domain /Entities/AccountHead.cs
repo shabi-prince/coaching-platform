c2be4acee07a8255c6b1cc3f16f34ee856bb8677
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 16:51:32 +0500

    Create AccountHead.cs

---
+++ Domain /Entities/AccountHead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class AccountHead : GenericModel
+    {
+        public string AccountHeadName { get; set; }
+        public string? AccountHeadDescription { get; set; }
+        public virtual IEnumerable<AccountCodes> AccountCodes { get; set; }
+    }
+}
