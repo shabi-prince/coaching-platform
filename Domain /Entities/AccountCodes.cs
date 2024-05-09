00fae129e4949e1ce8a238e56c442613d0d8b7bb
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 16:52:22 +0500

    Create AccountCodes.cs

---
+++ Domain /Entities/AccountCodes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class AccountCodes : GenericModel
+    {
+        [ForeignKey("AccountHead")]
+        public int AccountHeadId { get; set; }
+        public string AccountCode { get; set; }
+        public string AccountName { get; set; }
+        public virtual AccountHead? AccountHead { get; set; }
+        public virtual List<PaymentVoucher>? PaymentVouchers { get; set; }
+        public virtual List<ReceiptVoucher>? ReceiptVoucher { get; set; }
+
+    }
+}
