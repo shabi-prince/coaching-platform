6915b06ff375494a03d50b546c59dbf3a6362f1c
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 16:57:42 +0500

    Create ItemPurchaseOrderDTO.cs

---
+++ Domain /DTO /ItemPurchaseOrderDTO.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class ItemPurchaseOrderDTO
+    {
+        public int OrderId { get; set; }
+        public string PurchaseOrderDate { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public string PurchaseOrderCode { get; set; }
+        public string Vendor { get; set; }
+        public int? ItemCategoryId { get; set; }
+        public string Items { get; set; }
+        public bool IsReceipt { get; set; }
+        public IEnumerable<ItemPurchaseGoods> itemPurchaseGoods { get; set; }
+    }
+}
