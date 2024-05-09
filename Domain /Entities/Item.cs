d5bf8b3e5a5c642e9e4e02a6cf23c49c717de78f
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-09 16:53:12 +0500

    Create Item.cs

---
+++ Domain /Entities/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Item : GenericModel
+    {
+        public string ItemName { get; set; }
+        public string ItemCode { get; set; }
+        [ForeignKey("ItemCategory")]
+        public int? ItemCategoryId { get; set; }
+        [ForeignKey("MedicineType")]
+        public int? MedicineTypeId { get; set; }
+        public int? Stock { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Cost { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal? Price { get; set; }
+        public virtual ItemCategory? ItemCategory { get; set; }
+        public IEnumerable<ItemPurchaseGoods>? ItemPurchaseGoods { get; set; }
+        public IEnumerable<ItemSaleGoods>? ItemSaleGoods { get; set; }
+
+    }
+}
