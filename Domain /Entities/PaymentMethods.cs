2fc56bff611b490821e5fe2a6097327626cb46d3
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-10 11:18:34 +0500

    Create PaymentMethods.cs

---
+++ Domain /Entities/PaymentMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class PaymentMethods
+    {
+        [Key]
+        public int id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public string DeletedBy { get; set; }
+        public bool IsActive { get; set; } = true;
+        public string PM_Name { get; set; }
+        public IEnumerable<CustomerPayment> CustomerPayments { get; set; }
+    }
+}
