73747ef1538308ed106cc194aee2a02e63aea831
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-10 11:17:54 +0500

    Create NavigationMenu.cs

---
+++ Domain /Entities/NavigationMenu.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	[Table(name: "AspNetNavigationMenu")]
+	public class NavigationMenu
+    {
+		[Key]
+		//[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		[ForeignKey("ParentNavigationMenu")]
+		public int? ParentMenuId { get; set; }
+
+		public virtual NavigationMenu ParentNavigationMenu { get; set; }
+
+		public string? Area { get; set; }
+
+		public string? ControllerName { get; set; }
+
+		public string? ActionName { get; set; }
+
+		public bool IsExternal { get; set; } = false;
+
+        public string? ExternalUrl { get; set; }
+
+		public int DisplayOrder { get; set; }
+
+		[NotMapped]
+		public bool Permitted { get; set; } = true;
+
+        public bool IsButton { get; set; } = false;
+
+		public bool Visible { get; set; } = true;
+    }
+}
