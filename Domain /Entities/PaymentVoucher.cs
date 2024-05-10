4e8afedfaf452e8c4246ce195c30146f7deffccb
shoaib <79337459+shabi-prince@users.noreply.github.com>
2024-05-10 11:19:20 +0500

    Create PaymentVoucher.cs

---
+++ Domain /Entities/PaymentVoucher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class PaymentVoucher : GenericModel
+    {
+        public string VoucherCode { get; set; }
+        public DateTime VoucherDate { get; set; }
+        [ForeignKey("AccountCodes")]
+        public int AccountCodeId { get; set; }
+        [ForeignKey("PaymentMethods")]
+        public int PaymentMethod_id { get; set; }
+        public double VoucherAmount { get; set; }       
+        public virtual IEnumerable<PaymentVoucherDetail> PaymentVoucherDetails { get; set; }
+
+    }
+}
